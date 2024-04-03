66fa1b488d3d2d43145bd5b0515106adea850e09
tajwarsaiyeed <rjabid36@gmail.com>
2024-04-03 22:21:18 +0600

    Contest 3-4-2024

---
+++ Contest/3-4-2024/Reach 5 Star.cpp
@@ -0,0 +1,26 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-04-03 21:17:59
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define nl cout << '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define fr(i, n) for (int i = 0; i < n; i++)
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int x, y;
+    cin >> x >> y;
+    if (x + y >= 2000)
+        yes;
+    else
+        no;
+    return 0;
+}
