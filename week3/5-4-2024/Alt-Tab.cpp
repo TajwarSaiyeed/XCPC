5f852fdafd20c2b47b7aff19d9b4d33d50baa380
tajwarsaiyeed <rjabid36@gmail.com>
2024-04-05 23:06:02 +0600

    5-4-2024

---
+++ week3/5-4-2024/Alt-Tab.cpp
@@ -0,0 +1,21 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-04-05 23:04:57
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define nl cout << '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define fr(i, n) for (int i = 0; i < n; i++)
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
