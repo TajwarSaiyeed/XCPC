d5def2e7d11aca7fbb6c7c41570f582bd198f064
tajwarsaiyeed <rjabid36@gmail.com>
2024-04-07 23:11:28 +0600

    7-4-2024

---
+++ week4/7-4-2024/G - Matryoshkas.cpp
@@ -0,0 +1,23 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-04-07 23:08:27
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define nl cout << '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define fr(i, ivalue, n) for (int i = ivalue; i < n; i++)
+#define nfr(i, ivalue, n) for (int i = ivalue; i > n; i--)
+#define srtG(vll) sort(a.begin(), a.end(), greater<ll>())
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
