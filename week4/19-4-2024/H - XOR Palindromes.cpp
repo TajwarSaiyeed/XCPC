5414e4e0912a7dbe71f569ae6167df0cad09cf01
tajwarsaiyeed <rjabid36@gmail.com>
2024-04-19 23:26:32 +0600

    19-4-2024

---
+++ week4/19-4-2024/H - XOR Palindromes.cpp
@@ -0,0 +1,30 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-04-19 23:25:33
+    File: H - XOR Palindromes.cpp
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define yes cout << "YES"<<'\n'
+#define no cout << "NO"<<'\n'
+#define p1 cout << '1'
+#define p0 cout << '0'
+#define nl cout << '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define vs vector<string>
+#define fr(i, ivalue, n) for (int i = ivalue; i < n; i++)
+#define nfr(i, ivalue, n) for (int i = ivalue; i > n; i--)
+#define srtG(vll) sort(a.begin(), a.end(), greater<ll>())
+#define mps map<string, int>
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    return 0;
+}
+
+// Problem : https://codeforces.com/problemset/problem/1867/B
