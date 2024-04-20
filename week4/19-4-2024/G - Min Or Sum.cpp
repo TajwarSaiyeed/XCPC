6ccac30f4943320e7f3eb50a800c04ec30649cb2
tajwarsaiyeed <rjabid36@gmail.com>
2024-04-20 07:37:11 +0600

    19-4-2024

--- week4/19-4-2024/G - Min Or Sum.cpp
+++ week4/19-4-2024/G - Min Or Sum.cpp
@@ -24,7 +24,24 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        ll ans = 0, n;
+        cin >> n;
+        vi a(n);
+        fr(i, 0, n)
+        {
+            cin >> a[i];
+            ans |= a[i];
+        }
+
+        cout << ans << '\n';
+    }
+
     return 0;
 }
 
 // Problem : https://codeforces.com/problemset/problem/1635/A
+// Submission : https://vjudge.net/solution/50760702
--- week4/19-4-2024/H - XOR Palindromes.cpp
+++ week4/19-4-2024/H - XOR Palindromes.cpp
@@ -5,8 +5,8 @@
 */
 #include <bits/stdc++.h>
 using namespace std;
-#define yes cout << "YES"<<'\n'
-#define no cout << "NO"<<'\n'
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
 #define p1 cout << '1'
 #define p0 cout << '0'
 #define nl cout << '\n'
@@ -23,8 +23,65 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        ll n;
+        cin >> n;
+        string s;
+        cin >> s;
+
+        ll minOneCount = 0, pairCount = 0, l = 0, r = n - 1;
+        // fr(i, 0, n)
+        // {
+        //     if (s[i] == s[n - i - 1] && i < n - i - 1)
+        //         // cout << i << n - i - 1 << "- p - " << s[i] << ' ' << s[n - i - 1] << '\n';
+        //         pairCount += 2;
+        //     else
+        //         minOneCount++;
+        // }
+        while (l < r)
+        {
+            if (s[l] == s[r])
+                pairCount += 2;
+            else
+                minOneCount++;
+            l++;
+            r--;
+        }
+
+        string ans = "";
+
+        fr(i, 0, n + 1)
+        {
+            ll total = i;
+            total -= minOneCount;
+
+            if (total >= 0)
+            {
+                ll remaining = total - pairCount, mid = total % 2;
+
+                total = max(mid, remaining);
+                total = max(0LL, total - (n % 2));
+
+                if (total == 0)
+                    ans.push_back('1');
+                else
+                    ans.push_back('0');
+            }
+            else
+            {
+                ans.push_back('0');
+            }
+        }
+
+        cout << ans << '\n';
+    }
+
     return 0;
 }
 
 // Problem : https://codeforces.com/problemset/problem/1867/B
+// Submission : https://vjudge.net/solution/50761146
