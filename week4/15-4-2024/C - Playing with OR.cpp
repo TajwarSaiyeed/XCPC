bb586bca44f6620759b67a81cc3c8759339218ba
tajwarsaiyeed <rjabid36@gmail.com>
2024-04-23 23:10:12 +0600

    15-4-2024

--- week4/15-4-2024/C - Playing with OR.cpp
+++ week4/15-4-2024/C - Playing with OR.cpp
@@ -5,8 +5,8 @@
 */
 #include <bits/stdc++.h>
 using namespace std;
-#define yes cout << "YES"<<'\n'
-#define no cout << "NO"<<'\n'
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
 #define p1 cout << '1'
 #define p0 cout << '0'
 #define nl cout << '\n'
@@ -23,6 +23,32 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    
+    ll tc;
+    cin >> tc;
+    while (tc--)
+    {
+        ll k, n, ans = 0;
+        cin >> k >> n;
+        vll a(k + 1), b(k + 1);
+        fr(i, 1, k + 1) cin >> a[i];
+
+        fr(i, 1, k + 1)
+        {
+            if (a[i] % 2)
+            {
+                b[i] = 1;
+            }
+            b[i] += b[i - 1];
+        }
+
+        fr(i, 1, k - n + 2) if (b[i + n - 1] - b[i - 1] > 0)
+            ans++;
+
+        cout << ans << '\n';
+    }
+
     return 0;
 }
+
+// Problem : https://www.codechef.com/problems/FIZZBUZZ2304
+// Submission : https://vjudge.net/solution/50850997
