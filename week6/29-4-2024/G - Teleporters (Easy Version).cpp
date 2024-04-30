a7de9dc382784b8b0662581d125c568b04042aa3
tajwarsaiyeed <rjabid36@gmail.com>
2024-04-30 20:02:14 +0600

    29-4-2024

--- week6/29-4-2024/G - Teleporters (Easy Version).cpp
+++ week6/29-4-2024/G - Teleporters (Easy Version).cpp
@@ -9,12 +9,13 @@ using namespace std;
 #define no cout << "NO" << '\n'
 #define p1 cout << '1'
 #define p0 cout << '0'
-#define nl cout << '\n'
+#define nl '\n'
 #define vll vector<ll>
 #define vi vector<int>
 #define vs vector<string>
 #define fr(i, ivalue, n) for (int i = ivalue; i < n; i++)
 #define nfr(i, ivalue, n) for (int i = ivalue; i > n; i--)
+#define srt(vll) sort(a.begin(), a.end())
 #define srtG(vll) sort(a.begin(), a.end(), greater<ll>())
 #define mps map<string, int>
 #define dt(n) fixed << setprecision(n)
@@ -25,5 +26,34 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        ll n, c;
+        cin >> n >> c;
+        vll a(n + 1);
+        fr(i, 1, n + 1)
+        {
+            cin >> a[i];
+            a[i] += i;
+        }
+        srt(a);
+        ll sum = 0, ans = 0;
+        fr(i, 1, n + 1)
+        {
+            if (sum + a[i] > c)
+                break;
+            sum += a[i];
+            ans++;
+        }
+
+        cout << ans;
+        nl;
+    }
+
     return 0;
 }
+
+// Problem : https://codeforces.com/problemset/problem/1791/G1
+// Submission : https://vjudge.net/solution/51018209
--- week6/29-4-2024/H - Negatives and Positives.cpp
+++ week6/29-4-2024/H - Negatives and Positives.cpp
@@ -25,5 +25,33 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        vll a(n);
+        set<ll> x;
+        ll ans = 0;
+        ll cnt = 0;
+        fr(i, 0, n)
+        {
+            cin >> a[i];
+            if (a[i] < 0)
+                cnt++;
+            x.insert(abs(a[i]));
+            ans += abs(a[i]);
+        }
+
+        if (cnt % 2 == 1)
+            cout << ans - (2 * *x.begin()) << '\n';
+        else
+            cout << ans << '\n';
+    }
+
     return 0;
 }
+
+// Problem : https://codeforces.com/problemset/problem/1791/E
+// Submission : https://vjudge.net/solution/51007874
