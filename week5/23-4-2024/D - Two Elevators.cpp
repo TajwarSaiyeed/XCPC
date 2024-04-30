c561f9e02f66431661e56a16dbb8dfa626597f46
tajwarsaiyeed <rjabid36@gmail.com>
2024-04-30 20:02:04 +0600

    23-4-2024

--- week5/23-4-2024/D - Two Elevators.cpp
+++ week5/23-4-2024/D - Two Elevators.cpp
@@ -9,7 +9,7 @@ using namespace std;
 #define no cout << "NO" << '\n'
 #define p1 cout << '1'
 #define p0 cout << '0'
-#define nl cout << '\n'
+#define nl '\n'
 #define vll vector<ll>
 #define vi vector<int>
 #define vs vector<string>
@@ -24,7 +24,26 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        ll a, b, c;
+        cin >> a >> b >> c;
+
+        ll x1 = abs(a - 1);
+        ll x2 = abs(b - c) + abs(c - 1);
+
+        if (x1 == x2)
+            cout << 3 << nl;
+        else if (x1 > x2)
+            cout << 2 << nl;
+        else
+            cout << 1 << nl;
+    }
+
     return 0;
 }
 
 // Problem : https://codeforces.com/problemset/problem/1729/A
+// Submission : https://vjudge.net/solution/51018361
--- week5/23-4-2024/N - Three Indices.cpp
+++ week5/23-4-2024/N - Three Indices.cpp
@@ -9,7 +9,7 @@ using namespace std;
 #define no cout << "NO" << '\n'
 #define p1 cout << '1'
 #define p0 cout << '0'
-#define nl cout << '\n'
+#define nl '\n'
 #define vll vector<ll>
 #define vi vector<int>
 #define vs vector<string>
@@ -24,7 +24,32 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        ll n;
+        cin >> n;
+        vll a(n + 1);
+        fr(i, 1, n + 1) cin >> a[i];
+
+        bool flag = true;
+        fr(i, 2, n)
+        {
+            if (a[i] > a[i - 1] && a[i] > a[i + 1])
+            {
+                yes;
+                cout << i - 1 << " " << i << " " << i + 1 << nl;
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag)
+            no;
+    }
     return 0;
 }
 
 // Problem : https://codeforces.com/problemset/problem/1380/A
+// Submission : https://vjudge.net/solution/51020030
