49098c98a9dd99be634b2bb12c15fe0aa6d4e74e
tajwarsaiyeed <rjabid36@gmail.com>
2024-05-01 23:45:49 +0600

    Contest 1-5-2024

---
+++ Contest/1-5-2024/Change Please.cpp
@@ -0,0 +1,41 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-05-01 20:58:00
+    File: Change Please.cpp
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define p1 cout << '1'
+#define p0 cout << '0'
+#define nl '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define vs vector<string>
+#define fr(i, ivalue, n) for (int i = ivalue; i < n; i++)
+#define nfr(i, ivalue, n) for (int i = ivalue; i > n; i--)
+#define srt(vll) sort(a.begin(), a.end())
+#define srtG(vll) sort(a.begin(), a.end(), greater<ll>())
+#define mps map<string, int>
+#define dt(n) fixed << setprecision(n)
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int x;
+        cin >> x;
+        int y = 100 - x;
+        int z = y / 10;
+        cout << 10 * z << '\n';
+    }
+
+    return 0;
+}
---
+++ Contest/1-5-2024/ICE CREAM.cpp
@@ -0,0 +1,37 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-05-01 20:52:30
+    File: ICE CREAM.cpp
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define p1 cout << '1'
+#define p0 cout << '0'
+#define nl '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define vs vector<string>
+#define fr(i, ivalue, n) for (int i = ivalue; i < n; i++)
+#define nfr(i, ivalue, n) for (int i = ivalue; i > n; i--)
+#define srt(vll) sort(a.begin(), a.end())
+#define srtG(vll) sort(a.begin(), a.end(), greater<ll>())
+#define mps map<string, int>
+#define dt(n) fixed << setprecision(n)
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int x, y;
+    cin >> x >> y;
+    if (x * 2 <= y)
+        yes;
+    else
+        no;
+
+    return 0;
+}
