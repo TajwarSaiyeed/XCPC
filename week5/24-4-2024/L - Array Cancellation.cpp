d15cdcb79364bf62bc74df6a29601b34aafb9274
tajwarsaiyeed <rjabid36@gmail.com>
2024-05-01 06:33:35 +0600

    24-4-2024

--- week5/24-4-2024/L - Array Cancellation.cpp
+++ week5/24-4-2024/L - Array Cancellation.cpp
@@ -24,7 +24,35 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        ll n;
+        cin >> n;
+        vll a(n);
+        ll total = 0;
+        ll neg = 0;
+        fr(i, 0, n)
+        {
+            cin >> a[i];
+            if (a[i] >= 0)
+                total += a[i];
+            else if (a[i] + total >= 0)
+                total += a[i];
+            else
+            {
+                a[i] += total;
+                neg += a[i];
+                total = 0;
+            }
+        }
+
+        cout << abs(neg) << '\n';
+    }
+
     return 0;
 }
 
 // Problem : https://codeforces.com/problemset/problem/1405/B
+// Submission : https://vjudge.net/solution/51024894
