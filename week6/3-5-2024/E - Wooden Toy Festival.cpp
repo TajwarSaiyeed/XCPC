890b187d3b2a4b33e487a6d510d8e784a1f00016
tajwarsaiyeed <rjabid36@gmail.com>
2024-05-03 21:09:10 +0400

    3-5-2024

---
+++ week6/3-5-2024/E - Wooden Toy Festival.cpp
@@ -0,0 +1,30 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-05-03 21:00:00
+    File: E - Wooden Toy Festival.cpp
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define p1 cout << '1'
+#define p0 cout << '0'
+#define nl '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define vs vector<string>
+#define fr(i, ivalue, n) for (int i = ivalue; i < n; i++)
+#define nfr(i, ivalue, n) for (int i = ivalue; i > n; i--)
+#define srt(vll) sort(a.begin(), a.end())
+#define srtG(vll) sort(a.begin(), a.end(), greater<ll>())
+#define mps map<string, int>
+#define dt(n) fixed << setprecision(n)
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
