8b4c672cca005905e13bab0bc6ddbd697414823d
tajwarsaiyeed <rjabid36@gmail.com>
2024-05-08 23:07:28 +0600

    Contest 8-5-2024

---
+++ Contest/8-5-2024/Legs on a Farm.cpp
@@ -0,0 +1,50 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-05-08 20:47:13
+    File: Legs on a Farm.cpp
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define p1 cout << '1'
+#define p0 cout << '0'
+#define nl '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define vs vector<string>
+#define fr(i, ivalue, n) for (int i = ivalue; i < n; i++)
+#define nfr(i, ivalue, n) for (int i = ivalue; i > n; i--)
+#define srt(vll) sort(a.begin(), a.end())
+#define srtG(vll) sort(a.begin(), a.end(), greater<ll>())
+#define mps map<string, int>
+#define dt(n) fixed << setprecision(n)
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc;
+    cin >> tc;
+
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        int ans = 0;
+        while (n >= 4)
+        {
+            n -= 4;
+            ans++;
+        }
+
+        if (n == 2)
+            ans++;
+
+        cout << ans << nl;
+    }
+
+    return 0;
+}
