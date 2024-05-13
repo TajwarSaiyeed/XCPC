c0d4d3c3fa3c21d589413948f0bd95c9ca88c41a
tajwarsaiyeed <rjabid36@gmail.com>
2024-05-13 18:33:32 +0600

    7-4-2024

--- week4/7-4-2024/G - Matryoshkas.cpp
+++ week4/7-4-2024/G - Matryoshkas.cpp
@@ -19,5 +19,41 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    int tc;
+    cin >> tc;
+
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        vll a(n);
+        map<ll, ll> mp;
+        fr(i, 0, n)
+        {
+            cin >> a[i];
+            mp[a[i]]++;
+        }
+
+        set<ll> s;
+        fr(i, 0, n)
+        {
+            s.insert(a[i]);
+            s.insert(a[i] + 1);
+        }
+
+        ll x = 0, ans = 0;
+        for (auto i : s)
+        {
+            ll y = mp[i];
+            ans += max(0LL, y - x);
+            x = y;
+        }
+
+        cout << ans << '\n';
+    }
+
     return 0;
 }
+
+// Problem : https://codeforces.com/problemset/problem/1790/D
+// Submission : https://vjudge.net/solution/51255173
