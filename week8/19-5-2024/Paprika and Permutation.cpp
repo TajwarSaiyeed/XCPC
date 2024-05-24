9469e006d5934057cebcffff1c65bfe910efa8f6
tajwarsaiyeed <rjabid36@gmail.com>
2024-05-24 20:26:30 +0600

    19-5-2024

--- week8/19-5-2024/Paprika and Permutation.cpp
+++ week8/19-5-2024/Paprika and Permutation.cpp
@@ -26,5 +26,58 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        ll n;
+        cin >> n;
+        vll a(n), v;
+        map<ll, ll> mp, mp1;
+        fr(i, 0, n)
+        {
+            cin >> a[i];
+            mp[a[i]]++;
+        }
+        for (auto it : mp)
+        {
+            ll x = it.first, y = it.second;
+            if (x <= n)
+            {
+                fr(j, 1, y) v.push_back(x);
+                mp1[x] = 1;
+            }
+            else
+                fr(j, 1, y + 1) v.push_back(x);
+        }
+
+        ll cnt = 1, ans = 0;
+        fr(i, 0, v.size())
+        {
+            fr(j, cnt, n + 1)
+            {
+                if (mp1[j] == 1)
+                    cnt++;
+                else
+                    break;
+            }
+
+            ll z = (v[i] - 1) / 2;
+            if (cnt <= z)
+            {
+                cnt++;
+                ans++;
+            }
+            else
+            {
+                ans = -1;
+                break;
+            }
+        }
+        cout << ans << endl;
+    }
+
     return 0;
 }
+
+// Submission :  https://vjudge.net/solution/51467632
