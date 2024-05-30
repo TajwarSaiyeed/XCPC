ee494b4354384ca1361a170c9bd0151ffb5855ba
tajwarsaiyeed <rjabid36@gmail.com>
2024-05-30 21:54:49 +0600

    30-5-2024 feat: Add sliding window median implementation

--- Week 10/30-5-2024/B - Sliding Window Median.cpp
+++ Week 10/30-5-2024/B - Sliding Window Median.cpp
@@ -4,7 +4,13 @@
     File: B - Sliding Window Median.cpp
 */
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
 #define yes cout << "YES" << '\n'
 #define no cout << "NO" << '\n'
 #define p1 cout << '1'
@@ -26,5 +32,37 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int l = 0, r = 0;
+    pbds<pair<int, int>> p;
+    while (r < n)
+    {
+        p.insert({a[r], r});
+        if (r - l + 1 == k)
+        {
+            int pos = k / 2;
+            if (k % 2 == 0)
+            {
+                pos--;
+            }
+            auto it = p.find_by_order(pos);
+            cout << it->first << " ";
+            p.erase({a[l], l});
+            l++;
+        }
+        r++;
+    }
+    cout << '\n';
+
     return 0;
 }
+
+// Problem : https://cses.fi/problemset/task/1076/
+// Submission : https://vjudge.net/solution/51564752
