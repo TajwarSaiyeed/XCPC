f3b9e6252672582bdd92b6871db8b875bbafcbc0
tajwarsaiyeed <rjabid36@gmail.com>
2024-05-30 21:48:02 +0600

    30-5-2024
    feat: Add new code files for sliding window median, inversion count, and string reversal

---
+++ Week 10/30-5-2024/B - Sliding Window Median.cpp
@@ -0,0 +1,30 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-05-30 21:47:06
+    File: B - Sliding Window Median.cpp
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define p1 cout << '1'
+#define p0 cout << '0'
+#define nl '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define vs vector<string>
+#define fr(i, ivalue, n) for (int i = ivalue; i < n; i++)
+#define nfr(i, ivalue, n) for (int i = ivalue; i > n; i--)
+#define srt(vll) sort(vll.begin(), vll.end())
+#define srtG(vll) sort(vll.begin(), vll.end(), greater<ll>())
+#define mps map<string, int>
+#define dt(n) fixed << setprecision(n)
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
