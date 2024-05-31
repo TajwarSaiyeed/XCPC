bee817241112467f447fecf2d5585fb0886c5bb9
tajwarsaiyeed <rjabid36@gmail.com>
2024-05-31 10:57:13 +0600

    30-5-2024

--- Week 10/30-5-2024/C - Inversion Count.cpp
+++ Week 10/30-5-2024/C - Inversion Count.cpp
@@ -4,7 +4,12 @@
     File: C - Inversion Count.cpp
 */
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 #define yes cout << "YES" << '\n'
 #define no cout << "NO" << '\n'
 #define p1 cout << '1'
@@ -25,6 +30,25 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        vi a(n);
+        fr(i, 0, n) cin >> a[i];
+        long long ans = 0;
+        pbds<int> p;
+        nfr(i, n - 1, -1)
+        {
+            ans += p.order_of_key(a[i]);
+            p.insert(a[i]);
+        }
+        cout << ans << '\n';
+    }
     return 0;
 }
+
+// Problem : https://www.spoj.com/problems/INVCNT/en/
+// Solution : https://vjudge.net/solution/51571452
--- Week 10/30-5-2024/H - String Reversal.cpp
+++ Week 10/30-5-2024/H - String Reversal.cpp
@@ -4,7 +4,12 @@
     File: H - String Reversal.cpp
 */
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 #define yes cout << "YES" << '\n'
 #define no cout << "NO" << '\n'
 #define p1 cout << '1'
@@ -26,5 +31,36 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    string r = s;
+    reverse(r.begin(), r.end());
+
+    map<char, vector<int>> pos_s, pos_r;
+    fr(i, 0, n)
+    {
+        pos_s[s[i]].push_back(i);
+        pos_r[r[i]].push_back(i);
+    }
+
+    vi permutation(n);
+    for (char ch = 'a'; ch <= 'z'; ch++)
+        fr(i, 0, pos_s[ch].size())
+            permutation[pos_r[ch][i]] = pos_s[ch][i];
+
+    pbds<int> p;
+    long long ans = 0;
+    nfr(i, n - 1, -1)
+    {
+        ans += p.order_of_key(permutation[i]);
+        p.insert(permutation[i]);
+    }
+    cout << ans << '\n';
+
     return 0;
 }
+
+// https://codeforces.com/problemset/problem/1430/E
+// Submission : https://vjudge.net/solution/51571354
