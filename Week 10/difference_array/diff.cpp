0683a11f8dd0eadcd3b01279376f5c6c5944d976
tajwarsaiyeed <rjabid36@gmail.com>
2024-06-01 21:54:04 +0600

    Add code file for difference array implementation

---
+++ Week 10/difference_array/diff.cpp
@@ -0,0 +1,51 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-06-01 11:52:10
+    File: diff.cpp
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define p1 cout << '1'
+#define p0 cout << '0'
+#define nl '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define vs vector<string>
+#define fr(i, ivalue, n) for (int i = ivalue; i < n; i++)
+#define nfr(i, ivalue, n) for (int i = ivalue; i > n; i--)
+#define srt(vll) sort(vll.begin(), vll.end())
+#define srtG(vll) sort(vll.begin(), vll.end(), greater<ll>())
+#define mps map<string, int>
+#define dt(n) fixed << setprecision(n)
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    vi a(n + 1);
+    fr(i, 1, n + 1) cin >> a[i];
+    vi d;
+    fr(i, 1, n + 1)
+    {
+        d.push_back(a[i] - a[i - 1]);
+    }
+
+    for (auto val : d)
+        cout << val << " ";
+
+    cout << nl;
+
+    return 0;
+}
