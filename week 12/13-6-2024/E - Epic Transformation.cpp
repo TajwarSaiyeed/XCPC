aa9af65a37adfa6eaf0733c1d63ca2059fc752c0
tajwarsaiyeed <rjabid36@gmail.com>
2024-06-13 23:04:42 +0600

    feat: Implement solution for problem D - Epic Transformation

---
+++ week 12/13-6-2024/E - Epic Transformation.cpp
@@ -0,0 +1,62 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-06-13 23:00:27
+    File: E - Epic Transformation.cpp
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define p1 cout << '1'
+#define p0 cout << '0'
+#define nl '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define vs vector<string>
+#define fr(i, ivalue, n) for (int i = ivalue; i < n; i++)
+#define nfr(i, ivalue, n) for (int i = ivalue; i > n; i--)
+#define srt(vll) sort(vll.begin(), vll.end())
+#define srtG(vll) sort(vll.begin(), vll.end(), greater<ll>())
+#define mps map<string, int>
+#define dt(n) fixed << setprecision(n)
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc;
+    cin >> tc;
+
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        map<int, int> mp;
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            mp[x]++;
+        }
+
+        int mx = 0;
+        for (auto i : mp)
+            mx = max(mx, i.second);
+
+        if (mx > n / 2)
+            cout << 2 * mx - n << '\n';
+        else
+            cout << n % 2 << '\n';
+    }
+
+    return 0;
+}
+
+// Problem : https://codeforces.com/problemset/problem/1506/D
