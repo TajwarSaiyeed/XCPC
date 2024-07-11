22a6acd3a5e401d4e27ccb5dafd08f339ea102d1
tajwarsaiyeed <rjabid36@gmail.com>
2024-07-11 22:06:34 +0600

    feat: Add code for problems A - Only Pluses, B - Angry Monk, and C - Gorilla and Permutation
    Codeforces Round 957 (Div. 3)

---
+++ Contest/Codeforces Round 957 (Div. 3)/B. Angry Monk.cpp
@@ -0,0 +1,52 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-07-11 21:02:05
+    File: B. Angry Monk.cpp
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define p1 cout << '1'
+#define p0 cout << '0'
+#define nl '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define vs vector<string>
+#define fr(i, ivalue, n) for (int i = ivalue; i < n; i++)
+#define nfr(i, ivalue, n) for (int i = ivalue; i > n; i--)
+#define srt(vll) sort(vll.begin(), vll.end())
+#define srtG(vll) sort(vll.begin(), vll.end(), greater<ll>())
+#define mps map<string, int>
+#define dt(n) fixed << setprecision(n)
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n, k;
+        cin >> n >> k;
+        vi a(k);
+        fr(i, 0, k) cin >> a[i];
+        srt(a);
+        int ans = 0;
+        fr(i, 0, k - 1) if (a[i] != 1) ans += a[i] * 2 - 1;
+        else ans++;
+        cout << ans << endl;
+    }
+
+    return 0;
+}
+
+// Problem : https://codeforces.com/contest/1992/problem/B
