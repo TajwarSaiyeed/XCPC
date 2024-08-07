e3394011a809aee7c3d62e84fdc60bfe825d8921
tajwarsaiyeed <rjabid36@gmail.com>
2024-08-07 21:27:20 +0600

    7-8-2024: Contest Starters 146 (Rated till 5 stars)

---
+++ Contest/7-8-2024 Starters 146 (Rated till 5 stars)/No Winner.cpp
@@ -0,0 +1,59 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-08-07 20:39:38
+    File: No Winner.cpp
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define p1 cout << '1'
+#define p0 cout << '0'
+#define nl '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define vs vector<string>
+#define fr(i, ivalue, n) for (int i = ivalue; i < n; i++)
+#define nfr(i, ivalue, n) for (int i = ivalue; i > n; i--)
+#define srt(vll) sort(vll.begin(), vll.end())
+#define srtG(vll) sort(vll.begin(), vll.end(), greater<ll>())
+#define mps map<string, int>
+#define dt(n) fixed << setprecision(n)
+typedef long long ll;
+
+void solve()
+{
+    int a, b, c, m;
+    cin >> a >> b >> c >> m;
+    int arr[3] = {a, b, c};
+    sort(arr, arr + 3);
+    int sum = a + b + c;
+    int x = sum - arr[2] - arr[0];
+    if (arr[2] - x > m && x - arr[0] > m)
+        no;
+    else
+        yes;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc = 1;
+    cin >> tc;
+
+    while (tc--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+// Problem : https://www.codechef.com/START146C/problems/NOWINNER
