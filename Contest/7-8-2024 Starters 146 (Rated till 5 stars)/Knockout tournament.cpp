c5cfcfb6c75c819226950df6b998d76be84b2f5a
tajwarsaiyeed <rjabid36@gmail.com>
2024-08-08 22:28:53 +0600

    7-8-2024 : Add code for Knockout tournament.cpp

---
+++ Contest/7-8-2024 Starters 146 (Rated till 5 stars)/Knockout tournament.cpp
@@ -0,0 +1,63 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-08-08 22:06:54
+    File: Knockout tournament.cpp
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define p1 cout << '1'
+#define p0 cout << '0'
+#define nl '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define vs vector<string>
+#define fr(i, ivalue, n) for (int i = ivalue; i < n; i++)
+#define nfr(i, ivalue, n) for (int i = ivalue; i > n; i--)
+#define srt(vll) sort(vll.begin(), vll.end())
+#define srtG(vll) sort(vll.begin(), vll.end(), greater<ll>())
+#define mps map<string, int>
+#define dt(n) fixed << setprecision(n)
+typedef long long ll;
+
+void solve()
+{
+    vector<pair<int, int>> p(16);
+    fr(i, 0, 16)
+    {
+        cin >> p[i].first;
+        p[i].second = i;
+    }
+    srt(p);
+    vi ans(16, 0);
+    fr(i, 0, 16)
+        ans[p[i].second] = log2(i + 1);
+
+    for (int val : ans)
+        cout << val << " ";
+    cout << nl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc = 1;
+    cin >> tc;
+
+    while (tc--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+// Problem : https://www.codechef.com/problems/KNOCKOUT
