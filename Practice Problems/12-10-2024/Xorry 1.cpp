604d1daa85a1cfaa85a1378248de16b3566ac1ce
tajwarsaiyeed <rjabid36@gmail.com>
2024-08-12 23:52:59 +0600

    feat: Add code for problems B. Rock and Lever.cpp and Xorry 1.cpp

---
+++ Practice Problems/12-10-2024/Xorry 1.cpp
@@ -0,0 +1,59 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-08-12 23:47:59
+    File: Xorry 1.cpp
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define p1 cout << '1'
+#define p0 cout << '0'
+#define nl '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define vs vector<string>
+#define fr(i, ivalue, n) for (int i = ivalue; i < n; i++)
+#define nfr(i, ivalue, n) for (int i = ivalue; i > n; i--)
+#define srt(vll) sort(vll.begin(), vll.end())
+#define srtG(vll) sort(vll.begin(), vll.end(), greater<ll>())
+#define mps map<string, int>
+#define dt(n) fixed << setprecision(n)
+typedef long long ll;
+
+void solve()
+{
+    ll x;
+    cin >> x;
+    ll cnt = 0, cpy = x;
+    while (x >= 2)
+    {
+        x /= 2;
+        cnt++;
+    }
+    ll ans = pow(2, cnt);
+    cout << cpy - ans << " " << ans << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc = 1;
+    cin >> tc;
+
+    while (tc--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+// Problem : https://www.codechef.com/problems/XORRY1
