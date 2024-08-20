10536f42752c2f06af873dac0054255dc10ed80b
tajwarsaiyeed <rjabid36@gmail.com>
2024-08-20 21:36:30 +0600

    feat: Add code for problems A. Make All Equal.cpp and B. Generate Permutation.cpp

---
+++ Contest/Codeforces Round 967 (Div. 2)/A. Make All Equal.cpp
@@ -0,0 +1,63 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-08-20 20:57:56
+    File: A. Make All Equal.cpp
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define p1 cout << '1'
+#define p0 cout << '0'
+#define nl '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define vs vector<string>
+#define fr(i, ivalue, n) for (int i = ivalue; i < n; i++)
+#define nfr(i, ivalue, n) for (int i = ivalue; i > n; i--)
+#define srt(vll) sort(vll.begin(), vll.end())
+#define srtG(vll) sort(vll.begin(), vll.end(), greater<ll>())
+#define mps map<string, int>
+#define dt(n) fixed << setprecision(n)
+typedef long long ll;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vi arr(n);
+    map<int, int> freq;
+    fr(i, 0, n)
+    {
+        cin >> arr[i];
+        freq[arr[i]]++;
+    }
+
+    int mx = 0;
+    fr(i, 1, n + 1) mx = max(mx, freq[i]);
+
+    cout << n - mx << nl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc = 1;
+    cin >> tc;
+
+    while (tc--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+// Problem : https://codeforces.com/contest/2001/problem/A
---
+++ Contest/Codeforces Round 967 (Div. 2)/B. Generate Permutation.cpp
@@ -0,0 +1,68 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-08-20 21:08:06
+    File: B. Generate Permutation.cpp
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define p1 cout << '1'
+#define p0 cout << '0'
+#define nl '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define vs vector<string>
+#define fr(i, ivalue, n) for (int i = ivalue; i < n; i++)
+#define nfr(i, ivalue, n) for (int i = ivalue; i > n; i--)
+#define srt(vll) sort(vll.begin(), vll.end())
+#define srtG(vll) sort(vll.begin(), vll.end(), greater<ll>())
+#define mps map<string, int>
+#define dt(n) fixed << setprecision(n)
+typedef long long ll;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    if (n % 2 == 0)
+        cout << -1 << nl;
+    else
+    {
+        int l = 1, r = n;
+        fr(i, 0, n) if (i % 2 == 0)
+        {
+            cout << l << " ";
+            l++;
+        }
+        else
+        {
+            cout << r << " ";
+            r--;
+        }
+    }
+    cout << nl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc = 1;
+    cin >> tc;
+
+    while (tc--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+// Problem : https://codeforces.com/contest/2001/problem/B
